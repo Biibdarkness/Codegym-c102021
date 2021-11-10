96d03e6a5740af291a7a8b2f5d06ff149fae4250
Biibdarkness <93692512+Biibdarkness@users.noreply.github.com>
2021-11-10 15:06:27 +0700

    useoperator

---
+++ Bai tap/useoperator.js
@@ -0,0 +1,10 @@
+var width = prompt('nhập chiều rộng:');
+var height = prompt('nhập chiều cao:');
+
+
+area = width * height;
+document.write('Chiều rộng là:' + width);
+document.write('<br>')
+document.write('chiều dài là:'+height)
+document.write('<br>')
+document.write('diện tích là:' + area)
