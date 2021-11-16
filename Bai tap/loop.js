3f1f209962f212f18f74732118e514a11bb8acd0
Biibdarkness <93692512+Biibdarkness@users.noreply.github.com>
2021-11-16 13:53:40 +0700

    bcc

--- Bai tap/loop.js
+++ Bai tap/loop.js
@@ -12,7 +12,38 @@
 //     i++;
 // } while (i<=10);
 
-var i = prompt('abc');
-while (i !== 0){
-    console.log(i);
-}
+// var i = prompt('abc');
+// while (i !== 0){
+//     console.log(i);
+// }
+// var count = 0;
+// while (count++ < 10) {
+//     count++;
+    
+// }
+// console.log(count)
+// let x = 1;
+// for (let i = 0; i < 3; i++) {
+//     x = x + 1;
+// }
+// console.log(x);
+// let i = 0;
+// while (i < 3) {
+//     console.log("hi");
+//     i++;
+// }
+// let x = 1;
+// for (let i = 0; i < -1; i = i + 2) {
+//     x = x + 1;
+// }
+// console.log(x);
+// let x = 1, i;
+// for (i = 0; i < 3; i = i + 2) {
+//     x = x + 1;
+// }
+// console.log(x);
+// console.log(i);
+// let i = 0;
+// while (i < 0) {
+//     console.log("hi");
+// }
